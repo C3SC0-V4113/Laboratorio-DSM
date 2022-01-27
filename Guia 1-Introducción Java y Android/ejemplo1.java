249d13d36dd46cd9be4c99d86ba1fd11b2f6c830
CESCO <cescovalledev@gmail.com>
2022-01-27 13:45:52 -0600

    Primero funcionando

--- Guia 1-Introducción Java y Android/ejemplo1.java
+++ Guia 1-Introducción Java y Android/ejemplo1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main{
+public class ejemplo1{
   public static void main(String[] args){
     //Creando un objeto de la clase Scanner
     Scanner in=new Scanner(System.in);
@@ -10,7 +10,7 @@ public class Main{
 
     //Obtener el segundo dato
     System.out.print("¿Cuántos años tienes?");
-    Int edad=int.nextInt();
+    int edad=in.nextInt();
 
     //Mostrar el resultado en la consola
     System.out.println("Hola, "+nombre+". Dentro de poco tendrás "+(edad+1)+" años");
