12fa6ae70c5f3769df9c159b07e57efad755f1dd
CESCO <cescovalledev@gmail.com>
2022-02-19 20:31:35 -0600

    Contador funcionando

--- Guia_2-Vida_Componentes_Permisos/Contador/app/src/main/java/sv/edu/udb/contador_vc190544/MainActivity.java
+++ Guia_2-Vida_Componentes_Permisos/Contador/app/src/main/java/sv/edu/udb/contador_vc190544/MainActivity.java
@@ -3,15 +3,45 @@ package sv.edu.udb.contador_vc190544;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv_cont;
-    
+    private Button btnCont;
+    private int cont;
+
+    private View.OnClickListener clickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Contador();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_cont=(TextView) findViewById(R.id.tv_cont);
+        btnCont=(Button) findViewById(R.id.btnCont);
+        btnCont.setOnClickListener(clickListener);
+
+        initCounter();
+    }
+
+    private void initCounter(){
+        cont=0;
+        tv_cont.setText(cont+"");
+    }
+
+    public void Contador(){
+        if (cont==9){
+            cont=0;
+        }else {
+            cont++;
+        }
+        tv_cont.setText(cont+"");
     }
 }
