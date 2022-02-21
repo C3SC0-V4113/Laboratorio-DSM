229444366db6bd5486adfc2c22b6895401492a5a
CESCO <cescovalledev@gmail.com>
2022-02-20 19:46:45 -0600

    Terminar guia-1

--- Guia_1-Introduccion_Java_Android/OperacionesApp/app/src/main/java/com/example/operacionesapp/MainActivity.java
+++ Guia_1-Introduccion_Java_Android/OperacionesApp/app/src/main/java/com/example/operacionesapp/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1=findViewById(R.id.et1);
-        et2=findViewById(R.id.et2);
-        tv1=findViewById(R.id.tv1);
+        et1=findViewById(R.id.et);
+        et2=findViewById(R.id.editt2);
+        tv1=findViewById(R.id.tv);
     }
 
     //Este método se ejecutará cuando se presione el botón
@@ -48,12 +48,12 @@ public class MainActivity extends AppCompatActivity {
         String valor2=et2.getText().toString();
         int nro1=Integer.parseInt(valor1);
         int nro2=Integer.parseInt(valor2);
-        int multiplicación=nro1*nro2;
-        String resu=String.valueOf(multiplicación);
+        int multiplicacion=nro1*nro2;
+        String resu=String.valueOf(multiplicacion);
         tv1.setText(resu);
     }
 
-    public void división(View view) {
+    public void division(View view) {
         String valor1=et1.getText().toString();
         String valor2=et2.getText().toString();
         int nro1=Integer.parseInt(valor1);
