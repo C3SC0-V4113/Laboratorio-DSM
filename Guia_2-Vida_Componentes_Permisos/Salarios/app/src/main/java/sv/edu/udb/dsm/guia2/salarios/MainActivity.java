c63d4d7a698a3f8a14839f47d52148235f6c42ea
CESCO <cescovalledev@gmail.com>
2022-03-05 19:41:48 -0600

    Segunda actividad agregada

--- Guia_2-Vida_Componentes_Permisos/Salarios/app/src/main/java/sv/edu/udb/dsm/guia2/salarios/MainActivity.java
+++ Guia_2-Vida_Componentes_Permisos/Salarios/app/src/main/java/sv/edu/udb/dsm/guia2/salarios/MainActivity.java
@@ -2,13 +2,45 @@ package sv.edu.udb.dsm.guia2.salarios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et_nombre;
+    private EditText et_horas;
+    private Button bt_res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_nombre=findViewById(R.id.et_name);
+        et_horas=findViewById(R.id.et_horas);
+        bt_res=findViewById(R.id.btn_calcular);
+
+        bt_res.setOnClickListener(clickListener);
+    }
+
+    //Observador para el botón
+    private View.OnClickListener clickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            /**
+             * Se ejecuta el método de contador. Independientemente del resultado
+             * se guarda el valor en el ViewModel
+             */
+            PasandoDatos(view);
+        }
+    };
+
+    private void PasandoDatos(View v){
+        Intent intent=new Intent(this,Resultado.class);
+        intent.putExtra("txtName",et_nombre.getText().toString());
+        intent.putExtra("txtHours",et_horas.getText().toString());
+        startActivity(intent);
     }
 }
