1a52a04da7d4b46cd7e7364ec128b5410fad8449
CESCO <cescovalledev@gmail.com>
2022-03-13 14:36:04 -0600

    Agregando las clases al primer ejercicio

---
+++ Guia_4-Login_RealTime_Firebase/LoginFirebase/app/src/main/java/sv/edu/udb/dsm/guia4/loginfirebase_vc190544/DashboardActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.dsm.guia4.loginfirebase_vc190544;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- Guia_4-Login_RealTime_Firebase/LoginFirebase/app/src/main/java/sv/edu/udb/dsm/guia4/loginfirebase_vc190544/MainActivity.java
+++ Guia_4-Login_RealTime_Firebase/LoginFirebase/app/src/main/java/sv/edu/udb/dsm/guia4/loginfirebase_vc190544/MainActivity.java
@@ -2,13 +2,33 @@ package sv.edu.udb.dsm.guia4.loginfirebase_vc190544;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn,loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews(){
+        registerBtn=findViewById(R.id.register);
+        loginBtn=findViewById(R.id.login);
     }
 }
