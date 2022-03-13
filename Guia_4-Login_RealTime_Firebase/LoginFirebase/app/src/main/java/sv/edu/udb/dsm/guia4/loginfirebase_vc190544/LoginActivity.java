3255657b36baa343eabde7f07c714997c569ace5
CESCO <cescovalledev@gmail.com>
2022-03-13 16:22:03 -0600

    Avanzada la MainActivity y tambien el LoginActivity

--- Guia_4-Login_RealTime_Firebase/LoginFirebase/app/src/main/java/sv/edu/udb/dsm/guia4/loginfirebase_vc190544/LoginActivity.java
+++ Guia_4-Login_RealTime_Firebase/LoginFirebase/app/src/main/java/sv/edu/udb/dsm/guia4/loginfirebase_vc190544/LoginActivity.java
@@ -1,14 +1,86 @@
 package sv.edu.udb.dsm.guia4.loginfirebase_vc190544;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailTV, passwordTV;
+    private Button loginBtn;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+    }
+    
+    private void loginUserAccount(){
+        progressBar.setVisibility(View.VISIBLE);
+        
+        String email,password;
+        email=emailTV.getText().toString();
+        password=passwordTV.getText().toString();
+        
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"Porfavor ingrese su correo...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(),"Porfavor ingrese su contraseña...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Inicio de Sesión Exitoso", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+
+                    Intent intent=new Intent(LoginActivity.this,DashboardActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "El Inicio de Sesión ha fallado! Porfavor, volver a intentar", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+    
+    private void initializeUI(){
+        emailTV=findViewById(R.id.email);
+        passwordTV=findViewById(R.id.password);
+        
+        loginBtn=findViewById(R.id.login);
+        progressBar=findViewById(R.id.progressBar);
     }
 }
