8124bd1af7d4ce93585da44542bdf6623139e3ac
CESCO <cescovalledev@gmail.com>
2022-04-27 15:36:10 -0600

    Desafio no completado pero que hueva intentarlo

---
+++ Desafio_Practico_2/Examen/app/src/main/java/com/udb/edu/desafio2/exmenesyevaluaciones_vc190544/AddEvaluacion.java
@@ -0,0 +1,14 @@
+package com.udb.edu.desafio2.exmenesyevaluaciones_vc190544;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEvaluacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_evaluacion);
+    }
+}
--- Desafio_Practico_2/Examen/app/src/main/java/com/udb/edu/desafio2/exmenesyevaluaciones_vc190544/DashboardAdmin.java
+++ Desafio_Practico_2/Examen/app/src/main/java/com/udb/edu/desafio2/exmenesyevaluaciones_vc190544/DashboardAdmin.java
@@ -2,7 +2,11 @@ package com.udb.edu.desafio2.exmenesyevaluaciones_vc190544;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardAdmin extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class DashboardAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
+
+        FloatingActionButton fab_agregar=findViewById(R.id.fab_agregar);
+
+        fab_agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getBaseContext(),AddEvaluacion.class);
+                startActivity(i);
+            }
+        });
     }
 }
