f190b1ad6791cb2ec8a9933961786742a9879742
CESCO <cescovalledev@gmail.com>
2022-05-14 11:51:18 -0600

    Iniciando guia 6

---
+++ Guia_6-Consumo_de_API_Rest-con-Retrofit/DogAPI-VC190544/app/src/main/java/com/udb/edu/guia6/dogapivc190544/MainActivity.java
@@ -0,0 +1,14 @@
+package com.udb.edu.guia6.dogapivc190544;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
